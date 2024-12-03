bfae0114ea4627e0fab2125480b4c1380424af10
Thaqif Mikail <oranqif35@gmail.com>
2024-12-04 07:06:54 +0800

    Updated

--- Home.js
+++ Home.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { StatusBar, Text, TouchableOpacity, View } from 'react-native';
+import {StyleSheet, StatusBar, Text, TouchableOpacity, View} from 'react-native';
+
+const styles = StyleSheet.create({
+    itemStyle: {
+        borderBottomWidth: 1,
+    },
+    textItemStyle: {
+        color: "whitesmoke",
+        fontWeight: "bold",
+        fontSize: 24,
+        textAlign: "center",
+        margin: 20,
+    }
+})
 
 const Home = ({ navigation }) => {
-    const Item = ({ itemName, itemNavigate }) => {
+    const Item = ({ style, itemName, itemNavigate }) => {
         return (
-            <TouchableOpacity onPress={() => {navigation.navigate(itemNavigate)}}>
-                <Text>{itemName}</Text>
-            </TouchableOpacity>
+            <View style={style}>
+                <TouchableOpacity onPress={() => {navigation.navigate(itemNavigate)}}>
+                    <Text style={styles.textItemStyle}>{itemName}</Text>
+                </TouchableOpacity>
+            </View>
         );
     };
 
     return (
         <View>
             <StatusBar/>
-            <Item itemName="General" itemNavigate="General"/>
-            <Item itemName="Grocery" itemNavigate="Grocery"/>
-            <Item itemName="Fitness" itemNavigate="Fitness"/>
+            <Item style={[styles.itemStyle, {backgroundColor: "#4c3c3c"}]} itemName="General" itemNavigate="General"/>
+            <Item style={[styles.itemStyle, {backgroundColor: "#4b5a46"}]} itemName="Grocery" itemNavigate="Grocery"/>
+            <Item style={[styles.itemStyle, {backgroundColor: "#465564"}]} itemName="Fitness" itemNavigate="Fitness"/>
         </View>
     );
 };
