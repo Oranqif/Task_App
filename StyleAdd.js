95f22f2839d55d3c534611c7c0879f21b311e463
Thaqif Mikail <oranqif35@gmail.com>
2024-12-04 04:57:31 +0800

    Updated

---
+++ StyleAdd.js
@@ -0,0 +1,73 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    separator: {
+        borderBottomWidth: 1,
+        borderColor: "darkgrey",
+    },
+    mainButtonStyle: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        margin: 10,
+    },
+    subButtonStyle: {
+        backgroundColor: "grey",
+        width: "70%",
+        borderRadius: 5,
+    },
+    textButtonStyle: {
+        color: "ghostwhite",
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "bold",
+        margin: 10,
+    },
+    inputStyle: {
+        backgroundColor: "#e6e6e6",
+        fontSize: 16,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: "#c8c8c8",
+        margin: 10
+    },
+    textInputStyle: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    mainStateButtonStyle: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        margin: 10,
+    },
+    subStateButtonStyle: {
+        backgroundColor: "grey",
+        width: "40%",
+        borderRadius: 5,
+    },
+    textStateButtonStyle: {
+        color: "ghostwhite",
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "bold",
+        margin: 10,
+    },
+    mainSubmitButtonStyle: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        margin: 10,
+    },
+    subSubmitButtonStyle: {
+        backgroundColor: "grey",
+        width: "70%",
+        borderRadius: 5,
+    },
+    textSubmitButtonStyle: {
+        color: "ghostwhite",
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "bold",
+        margin: 10,
+    },
+});
+
+export default styles;
